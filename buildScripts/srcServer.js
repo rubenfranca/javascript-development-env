8d26b9d884dd4a51d24cf2aa25290c478d0305db
rubenfranca <ruben_tiagof@hotmail.com>
2019-07-14 17:30:31 +0100

    Add linters

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,6 +17,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+/* eslint-disable no-console */
 app.listen(port, err => {
   if (err) {
     console.log(err);
