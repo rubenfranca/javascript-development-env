68e16efe798d521db1e7c1bf9a0f9d0e7e198c18
rubenfranca <ruben_tiagof@hotmail.com>
2019-07-14 20:46:58 +0100

    Add config

--- buildScripts/build.js
+++ buildScripts/build.js
@@ -1,13 +1,16 @@
+// More info on Webpack's Node API here: https://webpack.github.io/docs/node.js-api.html
+// Allowing console calls below since this is a build file.
+/*eslint-disable no-console */
 import webpack from 'webpack';
 import webpackConfig from '../webpack.config.prod';
 import chalk from 'chalk';
 
-process.env.NODE_ENV = 'production';
+process.env.NODE_ENV = 'production'; // this assures the Babel dev config doesn't apply.
 
 console.log(chalk.blue('Generating minified bundle for production. This will take a moment...'));
 
 webpack(webpackConfig).run((err, stats) => {
-  if (err) {
+  if (err) { // so a fatal error occurred. Stop here.
     console.log(chalk.red(err));
     return 1;
   }
@@ -25,9 +28,8 @@ webpack(webpackConfig).run((err, stats) => {
 
   console.log(`Webpack stats: ${stats}`);
 
-  // if we got this far, the build succeded.
-  console.log(chalk.green('Your app has been built for production and written to dist'));
+  // if we got this far, the build succeeded.
+  console.log(chalk.green('Your app has been built for production and written to /dist!'));
 
-  return 0
+  return 0;
 });
-
